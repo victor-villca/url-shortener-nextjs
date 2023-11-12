6f89f125af8945314df1fec0c7046141efd969a2
victor-villca <villcavictorleon@gmail.com>
2023-11-12 13:37:01 -0400

    input text stlyle and button component

--- src/components/ShortenBox.tsx
+++ src/components/ShortenBox.tsx
@@ -1,25 +1,28 @@
 import React from 'react'
 import {FaRegCopy} from 'react-icons/fa'
-import {BsFillArrowRightCircleFill} from 'react-icons/bs'
-
+import Button from './Button'
+import {BsArrowRight} from 'react-icons/bs'
 const ShortenBox = () => {
+  const iconSize = 25
   return (
     <div className='flex flex-col justify-center items-center gap-5'>
       <h1 className=' text-4xl font-bold text-center styleGradient
       '>Shorten Your Looooooong Links :3</h1>
       <p className=' text-lg text-lite-10 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus reiciendis architecto, totam maiores consequuntur molestias ipsam expedita repellendus quod? Fugiat.</p>
-      <form className=''>
-        <div className=' relative my-5 mx-auto w-full h-6 flex justify-between'>
-          <div className='absolute top-[40%] translate-y-[-50%]'>
-              <FaRegCopy />
+      <form className=' w-full'>
+        <div className=' relative my-5 mx-auto w-[350px] h-16 flex justify-between border-4 border-blue-500 rounded-full z-10 px-5
+         lg:w-[600px]
+
+        '>
+          <div className='absolute top-[50%] translate-y-[-50%] '>
+              <FaRegCopy  size = {iconSize}/>
           </div>
           <input type="text" placeholder='Enter the link here'
-            className='border h-full w-full outline-none text-lg pl-9 transition-all focus:text-gray-500'
+            className='h-full w-[90%] outline-none text-lg pl-9 transition-all focus:text-gray-500 bg-transparent '
           />
-          <button className='absolute top-[40%] translate-y-[-50%] right-3 '><BsFillArrowRightCircleFill /></button>
+          <Button icon= {BsArrowRight}/>
         </div>
       </form>
-
       <div>
         <div>
 
